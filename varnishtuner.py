a1d056957aa4dc260b3bb5ea758b9fe5dbb12329
root <root@cp6.unixy.net>
2015-03-15 00:48:45 +0300

    Varnish settings

--- varnishtuner.py
+++ varnishtuner.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
+# varnishtuner.py v0.1.0 - Varnish Cache tuner script
+# Copyright (C) 2015 Joe Hmamouche <joe@unixy.net>
 
-
-# If os arch is 64-bit increase number of idle threads (threads are cheap at startup. Linux schedulers are smarter now)
-
-import os,sys,optparse
+import os,sys,optparse,re
 from subprocess import Popen,PIPE
 from datetime import timedelta
 
@@ -27,9 +26,10 @@ class ServerMemory():
 		self.used_memory = self.usedMemory()
 
 	def free_rawoutput(self):
-		freecmd = """/bin/free -m"""
+		freecmd = """/usr/bin/free -m"""
 		free = Popen(freecmd, shell=True, stdout=PIPE)
-		freeout = free.wait().stdout.read()
+		free.wait()
+		freeout = free.stdout.read().split('\n')
 		return freeout
 
 	def freeMemory(self):
@@ -46,8 +46,10 @@ class ServerCPUInfo():
         def __init__(self):
                 self.cpuinfo_raw = self.cpuinfo_rawoutput()
                 self.nr_ht = self.numberHT()
+		self.have_ht = self.haveHT()
                 self.nr_cores = self.numberCores()
                 self.nr_live_threads = self.numberLiveThreads()
+		self.max_threads = self.maxAvailableThreads()
 
         def cpuinfo_rawoutput(self):
                 cpuinfocmd = """/bin/cat /proc/cpuinfo"""
@@ -94,12 +96,21 @@ class ServerCPUInfo():
 	# Some VMs have a subset of HT threads available. So even though
 	# CPUs * nr_cores > nr_ht, it doesn't mean vms have all those available
 	def maxAvailableThreads(self):
-		if self.numberHT() <= self.numberCores():
-			return self.numberHT()
+		return self.numberHT()
 
         def numberLiveThreads(self):
                 pass
 
+class VarnishOptions(dict):
+    def __init__(self, *args):
+        dict.__init__(self, args)
+
+    def __getitem__(self, key):
+        val = dict.__getitem__(self, key)
+        return val
+
+    def __setitem__(self, key, val):
+        dict.__setitem__(self, key, val)
 
 class VarnishStats(dict):
     def __init__(self, *args):
@@ -112,8 +123,90 @@ class VarnishStats(dict):
     def __setitem__(self, key, val):
         dict.__setitem__(self, key, val)
 
+class VarnishConfig():
+
+	def __init__(self, file):
+		self.all_options_text = self.getVarnishConfigText(file)
+		self.startupThreadCount = self.getStartupThreadCount(self.all_options_text)
+		self.maxThreadcount = self.getMaxThreadCount(self.all_options_text)
+		self.storageType = self.getVarnishStorageType(self.all_options_text)
+		self.memorySetting = self.getMemorySetting(self.all_options_text)
+		self.possibleMemUsage = self.getPossibleMemoryUsage()
+
+	def getVarnishConfigText(self,options_file):
+		try:
+			f = open(options_file, "r")
+		except:
+			msg_out("Unable to open file: " + options_file)
+			sys.exit(1)
+		configtext = f.read().split('\n')
+		return configtext
+
+
+	def getNumberThreadPools(self, options):
+		return 0
+
+	def getNumberThreadPoolMin(self, options):
+		for i in options:
+			if i.find("DAEMON_OPTS=") != -1:
+				val = re.match("thread_pool_min[^=]*=\D*(\d+)", i)
+		if val:
+			return val.group(1)
+		return 0
+
+	def getNumberThreadPoolMax(self, options):
+		return 0
+
+	def getStartupThreadCount(self, options):
+		return self.getNumberThreadPools(options) * self.getNumberThreadPoolMin(options)
+
+	def getMaxThreadCount(self, options):
+		return self.getNumberThreadPools(options) * self.getNumberThreadPoolMax(options)
+
+	def getVarnishStorageType(self, options):
+		for i in options:
+			if i.find("DAEMON_OPTS=") != -1:
+				if i.find("malloc") != -1:
+					return "Malloc"
+				if i.find("file") != -1:
+					return "File"
+				if i.find("persistent") != -1:
+					return "Persistent"
+				else:
+					return "Unknown"
+
+	# Memory field is free form (K, M, G, etc).
+	# Need to normalize to MB.
+	def getMemorySetting(self, options):
+		mem_str = ''
+		for i in options:
+			if i.find("DAEMON_OPTS=") != -1:
+				if i.find("malloc") != -1:
+					mem_str = i.split(',')[1].strip('"')
+				else:
+					mem_str = i.split(',')[2].strip('"')
+				break
+		mem_str = mem_str.lower()
+		if len(mem_str) > 0 and not mem_str.isalnum():
+			return int(mem_str) / (1024)
+
+		if mem_str.find('k') != -1 or mem_str.find('kb') != -1:
+			return int(int(mem_str.strip('k').strip('kb')) / 1024)
+		elif mem_str.find('m') != -1 or mem_str.find('mb') != -1:
+			return int(mem_str.strip('m').strip('mb'))
+		elif mem_str.find('g') != -1 or mem_str.find('gb') != -1:
+			return int(mem_str.strip('g').strip('gb')) * 1024
+		elif mem_str.find('t') != -1 or mem_str.find('tb') != -1:
+			return int(mem_str.strip('t').strip('tb')) * (1024*1024)
+
+	# There's roughly +10% overhead 
+	def getPossibleMemoryUsage(self):
+		return float(self.getMemorySetting(self.all_options_text)) * 1.10
+
 def VarnishStats1():
-	stat1cmd = varnish_statpath + """ -1 -f uptime,client_drop,backend_unhealthy,backend_fail,fetch_failed,n_wrk_failed,n_wrk_lqueue,n_wrk_queued,n_wrk_drop,n_expied,n_lru_nuked,n_objoverflow"""
+	stat1cmd = varnish_statpath + """ -1 -f uptime,client_drop,backend_unhealthy,""" + \
+			"""backend_fail,fetch_failed,n_wrk_failed,n_wrk_lqueue,""" + \
+			"""n_wrk_queued,n_wrk_drop,n_expied,n_lru_nuked,n_objoverflow"""
 	stat1 = Popen(stat1cmd, shell=True, stdout=PIPE)
 	stat1.wait()
 
@@ -160,16 +253,30 @@ def showBanner(vs):
 	showUptime(vs)
 	showNewline()
 
-
-# Check n_lru_nuked
+# Forceful eviction of objects from cache to make room
+# for others.
 def isObjectEvicted(vs):
-	pass
+	return (int(vs['n_lru_nuked']) > 0)
 
-# Check backend counters
+# Check backend fail counters
 def isBackendFrail(vs):
-	pass
+	return (int(vs['backend_drop']) > 0)
 
 # Check n_wrk_lqueue, n_wrk_queued, n_wrk_failed
+
+# Need to warn about queued work
+def isWrkQueueGrowing(vs):
+	return (int(vs['n_wrk_queued']) > 0)
+
+# Things aren't going well with caching
+# if this is true
+def isRequestDrop(vs):
+	return (int(vs['n_wrk_drop']) > 0)
+
+# Varnish had to drop client request due to resource
+# shortage
+def isClientDropped(vs):
+	return (int(vs['client_drop']) > 0)
 
 # Locate varnish's prefix path if possible and its binaries' location
 # Credit: http://stackoverflow.com/questions/377017/test-if-executable-exists-in-python
@@ -191,8 +298,8 @@ def which(program):
 
     return None
 
-__version__ = '0.1'
-usage = "Aha"
+__version__ = '0.1.0'
+usage = 'usage'
 
 # dmidecode doesn't do much in VZ envinronments
 def isVZ():
@@ -211,6 +318,7 @@ def arch_type():
 
 parser = optparse.OptionParser(usage=usage, version=__version__)
 parser.add_option('-n', help='Varnish installation base directory')
+parser.add_option('-o', help='Varnish options file (ex: -s /etc/sysconfig/varnish)')
 # parser.print_help()
 
 if os.path.isdir("/usr/local/varnish/bin"):
@@ -223,5 +331,9 @@ else:
 	varnish_admpath		= varnish_binpath_default + "varnishadm"
 
 VS = VarnishStats1()
+SCI = ServerCPUInfo()
+SM = ServerMemory()
+VC = VarnishConfig("/etc/sysconfig/varnish")
 
 showBanner(VS)
+# showVarnishSettings(VS)
